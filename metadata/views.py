2372b219f0df2ea73861ff80b8369954f74ed074
Faizan-invozone <muhammad.faizan@invozone.com>
2020-12-30 17:01:11 +0500

    function for getting metadata is made for farhan

--- metadata/views.py
+++ metadata/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from databaseconnection.models import Integration
+import json
 
+
+def get_mysql_credentials(sql_dialect, source):
+    if sql_dialect.name.lower() == 'mysql':
+        str_creds = source.credential
+        creds = json.loads(str_creds)
+        host = creds['host']
+        port = creds['port']
+        user = creds['user']
+        password = creds['password']
+        return host, port, user, password
+
+def get_mysql_metadata(integration):
+    host, port, user, password = get_mysql_credentials(integration.source.sql_dialect, integration.source)
+    a = 1
+    # your code will be here for fetching metadata
+    pass
 
 class ListMetaData(APIView):
     
@@ -11,6 +28,8 @@ class ListMetaData(APIView):
         if not integration_id:
             return Response(data={'error': 'Please provide integration ID'})
         integration = Integration.objects.filter(pk=integration_id)
-        if integration:
-            integration = integration[0]
+        if not integration:
+            return Response(data={'error': 'Integration not found'})
+        integration = integration[0]
+        metadata = get_mysql_metadata(integration)
         return Response(data={'data': 'integration found'})
