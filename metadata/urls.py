5463d7beafb4e12171858bdfd8aa48f9dcd35c95
Faizan-invozone <muhammad.faizan@invozone.com>
2021-01-13 17:02:33 +0500

    slash added in url

--- metadata/urls.py
+++ metadata/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('api/v1/get-metadata', ListMetaData.as_view()),
     path('api/v1/replicate-metadata', ReplicateMetaData.as_view()),
     path('api/v1/load-data', LoadDataIntoTarget.as_view()),
-    path('api/v1/load-api-data', LoadAPIDataToTarget.as_view()),
+    path('api/v1/load-api-data/', LoadAPIDataToTarget.as_view()),
 ]
